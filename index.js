53c6ff2e6207760fdcb68cbfa92e917d119ca19c
Na Lin <lna483018@gmail.com>
2023-01-02 22:21:42 -0500

    Password setup 3: google stratege options

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ const app = express();
 // todo 1: require modules
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20");
-
+const keys = require("./config/keys");
 // todo 2: infor passport how to use the strategy
 // 1. config -> customize how strategy behave inside of our app
 // 2. teach passport how to use this strategy
 // todo 3: enable google OAuth API to get clientID, clientSecret
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.GOOGLE_CLIENT_ID,
+      clientSecret: keys.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
 
 app.listen(5000);
