bae36bf0060d652d40ed8b361f8dfd96337b0984
Na Lin <lna483018@gmail.com>
2023-01-02 22:15:32 -0500

    Password setup 2: get clientID, clientSecret

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const GoogleStrategy = require("passport-google-oauth20");
 // todo 2: infor passport how to use the strategy
 // 1. config -> customize how strategy behave inside of our app
 // 2. teach passport how to use this strategy
+// todo 3: enable google OAuth API to get clientID, clientSecret
 passport.use(new GoogleStrategy());
 
 app.listen(5000);
