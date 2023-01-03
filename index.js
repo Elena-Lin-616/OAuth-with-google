373492c53053f1b0dbfee2bbecb50e9718c4e7c8
Na Lin <lna483018@gmail.com>
2023-01-02 21:57:04 -0500

    Passport setup step 1

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+// todo 1: require modules
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+
+// todo 2: infor passport how to use the strategy
+// 1. config -> customize how strategy behave inside of our app
+// 2. teach passport how to use this strategy
+passport.use(new GoogleStrategy());
 
 app.listen(5000);
