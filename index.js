3b9166c5abe868f0d68401eb1e2a9986fdeed50d
Na Lin <lna483018@gmail.com>
2023-01-03 17:01:26 -0500

    connect mongoDB

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 require("./services/passport");
 
 const app = express();
+
+mongoose.connect(keys.MONGOOSE_URL);
 
 require("./routes/authRoutes")(app);
 
