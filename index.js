8ee079e841b4e76142eb3d6ccb141527ffc72ea4
Na Lin <lna483018@gmail.com>
2023-01-03 21:10:50 -0500

    enable cookie by use cookie-session package

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
 const passport = require("passport");
 const session = require("express-session");
 const keys = require("./config/keys");
 require("./models/user");
 require("./services/passport");
 
-const app = express();
-
 mongoose.connect(keys.MONGOOSE_URL);
+
+const app = express();
 
 // required for passport
 app.use(session({ secret: "SECRET" })); // session secret
+
+// tell app to use cookie & passport to use cookie
+//  1.
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30d
+    keys: [keys.COOKIE_KEY],
+  })
+);
+// 2.
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 
